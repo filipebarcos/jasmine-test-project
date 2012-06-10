88f29ac8dc6e78d5b4c91887b4a3b8d0e57e182d
Filipe Costa <filipebarcos@gmail.com>
2012-06-10 11:28:56 -0300

    Make it green

--- js/spec/FibonacciSpec.js
+++ js/spec/FibonacciSpec.js
@@ -1,9 +1,13 @@
 describe("Fibonacci", function() {
 
-	var fibonacci
+	var fibonacci;
 
 	beforeEach(function() {
-		fibonacci = new Fibonacci()
-	})
+		fibonacci = new Fibonacci();
+	});
 
-})
+	it("should calculate 5th fibonacci sequence number", function () {
+		expect(fibonacci.execute(5)).toBe(5);
+	});
+
+});
--- js/src/Fibonacci.js
+++ js/src/Fibonacci.js
@@ -1,3 +1,9 @@
 function Fibonacci() {
 }
 
+Fibonacci.prototype.execute = function(value) {
+	if (value != 1 && value != 0 && value != 2) {
+		return this.execute(value - 1) + this.execute(value - 2);
+	}
+	return 1;
+};
