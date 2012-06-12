742c628f7f583636bf662402542e80cf47e55df1
Filipe Costa <filipebarcos@gmail.com>
2012-06-12 10:04:10 -0300

    Some fixes on Fibonacci function

--- js/spec/CalculatorSpec.js
+++ js/spec/CalculatorSpec.js
@@ -1,16 +1,16 @@
 describe("Calculator", function() {
 
-	var calculator;
+	var calculator
 
 	beforeEach(function() {
-		calculator = new Calculator();
-	});
+		calculator = new Calculator()
+	})
 
 	it("should calculate 5th fibonacci sequence number", function () {
-		expect(calculator.fibonacci(5)).toBe(5);
-	});
+		expect(calculator.fibonacci(5)).toBe(5)
+	})
 
 	it("should calculate factorial of 5", function() {
 		expect(calculator.factorial(5)).toBe(120)
-	});
-});
+	})
+})
--- js/src/Calculator.js
+++ js/src/Calculator.js
@@ -1,16 +1,17 @@
 function Calculator() {
 }
 
-Calculator.prototype.fibonacci = function(value) {
-	if (value != 1 && value != 0 && value != 2) {
-		return this.fibonacci(value - 1) + this.fibonacci(value - 2);
+Calculator.prototype.fibonacci = function(index) {
+	if (index > 1) {
+		return this.fibonacci(index - 1) + this.fibonacci(index - 2)
 	}
-	return 1;
+	
+	return index
 }
 
 Calculator.prototype.factorial = function(value) {
 	if (value > 1) {
-		return value * this.factorial(value - 1);
+		return value * this.factorial(value - 1)
 	}
-	return 1;
+	return 1
 }
